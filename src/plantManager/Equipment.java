43bcfd7622a61c1fb2653645da43b19eb6531eb3
glebsobolev <glebsobolev@gmail.com>
2022-07-02 11:22:05 +0400

    update

--- src/plantManager/Equipment.java
+++
@@ -1,6 +0,0 @@
-package plantManager;
-
-public abstract class Equipment {
-	private float x,y;
-	
-}
--- src/plantManager/Operator.java
+++
@@ -1,9 +0,0 @@
-package plantManager;
-
-import org.newdawn.slick.util.pathfinding.AStarPathFinder;
-
-public class Operator extends Person {
-
-	
-	
-}
